89234eebc150ae20b5c528dfb20c838040edde37
WashingtonSouza <thewashington.souza@gmail.com>
2017-06-27 15:57:36 -0300

    Classe Categoria e Produto criada.

--- Loja/Loja/Program.cs
+++ Loja/Loja/Program.cs
@@ -1,4 +1,8 @@
-﻿using Loja.INFRA;
+﻿using Loja.DAO;
+using Loja.INFRA;
+using Loja.Model;
+using NHibernate;
+using NHibernate.Cfg;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +16,39 @@ namespace Loja
         static void Main(string[] args)
         {
             NHibernateHelper.GeraSchema();
+
+
+
+            //ISession session = NHibernateHelper.AbreSession();
+
+            //UsuarioDAO usuarioDAO = new UsuarioDAO(session);
+            //Usuario novoUsuario = new Usuario();
+
+            //novoUsuario.Nome = "João";
+            //usuarioDAO.Adiciona(novoUsuario);
+            //session.Close();
+
+            Console.WriteLine("==============================================================");
+
+
+
+            Categoria umaCategoria = new Categoria();
+            umaCategoria.Nome = "Vestuario".ToUpper();
+
+            Produto produto = new Produto();
+            produto.Nome = "calça".ToUpper();
+            produto.Preco = 10;
+            //produto.Categoria = umaCategoria;
+            produto.Categoria = umaCategoria;
+
+            ISession session = NHibernateHelper.AbreSession();
+            ITransaction transacao = session.BeginTransaction();
+
+            session.Save(umaCategoria);
+            session.Save(produto);
+
+            transacao.Commit();
+            session.Close();
 
             Console.Read();
         }
